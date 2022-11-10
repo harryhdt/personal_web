e9c9ba6cfcbc774ee1bbfe65e469366071dd6465
Harry Hidayat <harryhdtofficial@gmail.com>
2022-11-10 07:17:38 +0700

    new app

--- components/Header.jsx
+++ components/Header.jsx
@@ -24,7 +24,7 @@ export function Header() {
   const [showMenu, setShowMenu] = useState(false);
   //
   useEffect(() => {
-    setMode(location.theme === "dark" ? "dark" : "light");
+    setMode(localStorage.theme === "dark" ? "dark" : "light");
     setShowMenu(window.innerWidth < 640 ? false : true);
     window.addEventListener("resize", () => {
       setShowMenu(window.innerWidth < 640 ? false : true);
