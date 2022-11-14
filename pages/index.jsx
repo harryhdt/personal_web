3dc5ab1da17a9f18aa0f8afcdd9013c56cd4dcce
Harry Hidayat <harryhdtofficial@gmail.com>
2022-11-14 12:09:38 +0800

    all

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ export default function Home() {
       title="Harry Hidayat"
       description="a Junior Full Stack Software Engineer, focused on Web and Mobile development. With more than 2 years experience"
     >
-      <div className="px-4 sm:px-8 md:px-16 lg:px-4 border-b border-zinc-200 dark:border-zinc-800 transition-colors">
+      <div className="px-4 sm:px-8 md:px-16 lg:px-4 transition-colors">
         <div className="w-full max-w-5xl mx-auto">
           <div className="h-[calc(100vh-111px)] min-h-[500px] flex items-center justify-center">
             <div className="flex flex-col items-center -mt-20 relative">
